adfd43f932a7804b40efb13a6ce3920fd671b48d
houyunsheng <1850774893@qq.com>
2017-11-07 21:43:49 +0800

    哈哈

--- test1.py
+++ test1.py
@@ -72,12 +72,90 @@ import tensorflow as tf
 # w3=tf.Variable(tf.random_normal([2,3],stddev=1),name="w3")
 # w4=tf.Variable(tf.random_normal([2,2],stddev=1),name="w4")
 # tf.assign(w3,w4,validate_shape=False)
-w1=tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
-w2=tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
-x=tf.placeholder(tf.float32,shape=[1,2],name="input")
-a=tf.matmul(x,w1)
-y=tf.matmul(a,w2)
-sess=tf.Session()
-init_op=tf.global_variables_initializer()
-sess.run(init_op)
-print(sess.run(y,feed_dict={x:[[0.7,0.9]]}))
+# w1=tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
+# w2=tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
+# x=tf.placeholder(tf.float32,shape=(3,2),name="input")
+# a=tf.matmul(x,w1)
+# y=tf.matmul(a,w2)
+# sess=tf.Session()
+# init_op=tf.global_variables_initializer()
+# sess.run(init_op)
+# print(sess.run(y,feed_dict={x:[[0.7,0.9],[0.1,0.4],[0.5,0.8]]}))
+# cross_entropy=-tf.reduce_mean(y*tf.log(tf.clip_by_value(y,1e-10,1.0)))
+# learning_rate=0.001
+# train_step=tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+# sess.run(train_step)
+# from numpy.random import RandomState
+# batch_size=8
+# w1=tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
+# w2=tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
+# x=tf.placeholder(tf.float32,shape=(None,2),name='x-input')
+# y_=tf.placeholder(tf.float32,shape=(None,1),name='y-input')
+# a=tf.matmul(x,w1)
+# y=tf.matmul(a,w2)
+# cross_entropy=-tf.reduce_mean(y_*tf.log(tf.clip_by_value(y,1e-10,1)))
+# train_step=tf.train.AdamOptimizer(0.001).minimize(cross_entropy)
+# rdm=RandomState(1)
+# dataset_size=128
+# X=rdm.rand(dataset_size,2)
+# Y=[[int(x1+x2<1)] for x1,x2 in X]
+# with tf.Session() as sess:
+#     init_op=tf.global_variables_initializer()
+#     sess.run(init_op)
+#     print(sess.run(w1))
+#     print(sess.run(w2))
+#     STEPS=5000
+#     for i in range(STEPS):
+#         start=(i*batch_size)% dataset_size
+#         end=min(start+batch_size,dataset_size)
+#         sess.run(train_step,feed_dict={x:X[start:end],y_:Y[start:end]})
+#         if i%1000==0:
+#             total_cross_entropy=sess.run(cross_entropy,feed_dict={x:X,y_:Y})
+#             print("After %d training step(s),cross entropy on all data is %g"%(i,total_cross_entropy))
+#     print(sess.run(w1))
+#     print(sess.run(w2))
+# v=tf.constant([[1.0,2.0,3.0],[4.0,5.0,6.0]])
+# sess=tf.Session()
+# print(tf.clip_by_value(v,2.5,4.5).eval(session=sess))
+# v=tf.constant([1.0,2.0,3.0])
+# sess=tf.Session()
+# print(tf.log(v).eval(session=sess))
+# v1=tf.constant([[1.0,2.0],[3.0,4.0]])
+# v2=tf.constant([[5.0,6.0],[7.0,8.0]])
+# sess=tf.Session()
+# print(sess.run(v1*v2))
+# print(sess.run(tf.matmul(v1,v2)))
+# v=tf.constant([[1.0,2.0,3.0],[4.0,5.,6.]])
+# sess=tf.Session()
+# print(sess.run(tf.reduce_mean(v)))
+# v1=tf.constant([1.0,2.0,3.0,4.0])
+# v2=tf.constant([4.0,3.0,2.0,1.0])
+# sess=tf.InteractiveSession()
+# loss=tf.reduce_sum(tf.where(tf.greater(v1,v2),v1,v2))
+# print(sess.run(loss))
+# sess.close()
+# from numpy.random import RandomState
+# batch_size=8
+# x=tf.placeholder(tf.float32,shape=(None,2),name='x-input')
+# y_=tf.placeholder(tf.float32,shape=(None,1),name='y-input')
+# w1=tf.Variable(tf.random_normal([2,1],stddev=1,seed=1))
+# y=tf.matmul(x,w1)
+# loss_less=10
+# loss_more=1
+# loss=tf.reduce_sum(tf.where(tf.greater(y,y_),(y-y_)*loss_more,(y_-y)*loss_less))
+# train_step=tf.train.AdamOptimizer(0.001).minimize(loss)
+# rdm=RandomState(1)
+# data_size=128
+# X=rdm.rand(data_size,2)
+# Y=[[x1+x2+rdm.rand()/10.-0.05] for(x1,x2) in X]
+# with tf.Session() as sess:
+#     init_op=tf.global_variables_initializer()
+#     sess.run(init_op)
+#     STEPS=5000
+#     for i in range(STEPS):
+#         start=(i*batch_size)%data_size
+#         end=min(start+batch_size,data_size)
+#         sess.run(train_step,feed_dict={x:X[start:end],y_:Y[start:end]})
+#     print(sess.run(w1))
+
+
